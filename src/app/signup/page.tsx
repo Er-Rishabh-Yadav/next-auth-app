428deed2f1c7931df6081122c9c2dfc815e641b5
Rishabh Yadav <github.for.rishabh@gmail.com>
2023-08-15 16:23:55 +0530

    UI Updated

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -37,23 +37,37 @@ export default function SignupPage() {
         }
     },[user])
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-500 py-2">
-            <h1 className="fixed text-2xl  tracking-wider left-0 top-0 flex w-full justify-center border-b border-slate-300 bg-gradient-to-b from-slate-200 pb-6 pt-8 backdrop-blur-xl dark:border-slate-800 dark:bg-slate-800/30 dark:from-inherit uppercase ">{loading ? "Processing" : "Signup"}</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-800 py-2">
+            <h1 className="fixed text-2xl  tracking-wider left-0 top-0 flex w-full justify-center border-b border-slate-300 pb-6 pt-8  uppercase font-bold text-yellow-500 m-2">{loading ? "Processing" : "Sign up"}</h1>
             <hr />
-            <label className="" htmlFor="username">Username</label>
-            <input className="p-2 rounded-xl border border-gray-300" type="text" id="username" value={user.username} onChange={(e)=> setUser({...user,username:e.target.value})} placeholder="username"/>
-            
-            <label className="" htmlFor="email">Email</label>
-            <input className="p-2 border rounded-xl border-gray-300" type="text" id="email" value={user.email} onChange={(e)=> setUser({...user,email:e.target.value})} placeholder="Email"/>
-            
-            <label className="" htmlFor="password">Password</label>
-            <input className="p-2 border rounded-xl border-gray-300" type="password" id="password" value={user.password} onChange={(e)=> setUser({...user,password:e.target.value})} placeholder="Password"/>
 
-            <button className="p-2 border border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>
+            <div className="bg-gray-500 p-4 rounded">
+
+            <div className="flex flex-row justify-center items-center mb-2 ">
+            <label className="ml-2 text-white" htmlFor="username">Username :</label>
+            <input className="p-2 ml-2 rounded-xl border border-gray-300" type="text" id="username" value={user.username} onChange={(e)=> setUser({...user,username:e.target.value})} placeholder="Username"/>
+            </div>
+
+            <div className="flex flex-row justify-center items-center mb-2 focus:outline-none">
+            <label className="ml-8 text-white" htmlFor="email">Email :</label>
+            <input className="p-2 ml-5   border rounded-xl border-gray-300" type="text" id="email" value={user.email} onChange={(e)=> setUser({...user,email:e.target.value})} placeholder="Email"/>
+            </div>
+
+            <div className="flex flex-row justify-center items-center mb-2">
+            <label className="ml-2 text-white" htmlFor="password">Password :</label>
+            <input className="p-2 ml-4 border rounded-xl border-gray-300" type="password" id="password" value={user.password} onChange={(e)=> setUser({...user,password:e.target.value})} placeholder="Password"/>
+            </div>
+
+            <div className="flex flex-row justify-center items-center m-2">
+            <button className="p-2 border text-white border-gray-300 rounded-lg m-4 focus:outline-none focus:border-gray-300 hover:bg-green-500" onClick={onSignup}>
                 {button ? "Please fill all the fields" : "Signup"}
             </button>
+            </div>
 
+            <div className="flex flex-row justify-center items-center m-2">
             <Link className="text-blue-700" href="/login">visit Login Page?</Link>
+            </div>
+            </div>
             <Toaster>
                 {(t) => (
                     <ToastBar
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -28,12 +28,17 @@ export default function VerifyEmailPage() {
     },[token]);
 
     return (
-        <div className="">
-            <h1>Verify Email</h1>
-            <h2>{token ?`${token}`:"no token"}</h2>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-800 text-white py-2">
+            <div className="bg-gray-500 p-4 rounded">
 
-            {error && <p>There was an error verifying your email</p>}
-            {verified && <p>Your email has been verified</p>}
+            <div className="flex flex-col text-2xl justify-center items-center text-center font-bold text-yellow-500 tracking-wider uppercase">
+            <h1 className="inline-block p-4">Verify Email</h1>
+            </div>
+            <h2 className="text-gray-400 text-center bg-slate-200 p-1 m-4">{token ?`${token}`:"no token"}</h2>
+
+            {error && <p className="bg-red-500 m-5 p-4 rounded font-bold text-xl text-center">There was an error verifying your email</p>}
+            {verified && <p className="bg-green-500 m-5 p-4 rounded font-bold text-xl text-center">Your email has been verified</p>}
+            </div>
         </div>
     );
 }
